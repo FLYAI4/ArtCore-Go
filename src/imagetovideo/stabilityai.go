69c51c76a7c87c47639d0d5a7828c46b657f8916
robert-min <robertmin522@gmail.com>
2024-02-07 10:32:02 +0900

    feature: ADD imagetovideo/stabilityai postGenerateVideo.

--- src/imagetovideo/stabilityai.go
+++ src/imagetovideo/stabilityai.go
@@ -1,9 +1,14 @@
 package imagetovideo
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"image"
 	"image/jpeg"
+	"io"
+	"mime/multipart"
+	"net/http"
 	"os"
 	"path/filepath"
 
@@ -14,13 +19,15 @@ type VideoManager struct {
 	changeWidth    int
 	changeHeight   int
 	userFolderPath string
+	token          string
 }
 
-func NewVideoManager(userFolderPath string) *VideoManager {
+func NewVideoManager(userFolderPath string, token string) *VideoManager {
 	return &VideoManager{
 		changeWidth:    768,
 		changeHeight:   768,
 		userFolderPath: userFolderPath,
+		token:          token,
 	}
 }
 
@@ -28,7 +35,11 @@ func (vm *VideoManager) GenerateVideoContent() {
 	// resize image to 768 * 768
 	_, err := vm.resizeImage()
 	if err != nil {
-		fmt.Println("Resize image error.", err)
+		fmt.Println("Resize image error: ", err)
+	}
+	_, err = vm.postGenerateVideo()
+	if err != nil {
+		fmt.Println("Post generate video error: ", err)
 	}
 }
 
@@ -70,5 +81,93 @@ func (vm *VideoManager) resizeImage() (string, error) {
 	}
 	fmt.Println("The image has been successfully resized and saved.")
 
-	return filePath, err
+	return filePath, nil
+}
+
+func (vm *VideoManager) postGenerateVideo() (string, error) {
+	filePath := filepath.Join(vm.userFolderPath, "resized_img.jpg")
+
+	// open file
+	file, err := os.Open(filePath)
+	if err != nil {
+		fmt.Println("Can't open resized file.", err)
+		return "", err
+	}
+	defer file.Close()
+
+	// get fileInfo
+	fileInfo, err := file.Stat()
+	if err != nil {
+		fmt.Println("Can't get file info.", err)
+		return "", err
+	}
+
+	// read fileInfo
+	fileBytes := make([]byte, fileInfo.Size())
+	_, err = file.Read(fileBytes)
+	if err != nil {
+		fmt.Println("Can't read file info.", err)
+		return "", err
+	}
+
+	// put file data into buffer
+	body := &bytes.Buffer{}
+	writer := multipart.NewWriter(body)
+	formFile, err := writer.CreateFormFile("image", fileInfo.Name())
+	if err != nil {
+		fmt.Println("Can't create form file.", err)
+		return "", err
+	}
+	_, err = io.Copy(formFile, bytes.NewReader(fileBytes))
+	if err != nil {
+		fmt.Println("Can't put file data into buffer.", err)
+		return "", err
+	}
+	writer.Close()
+
+	// create request
+	url := "https://api.stability.ai/v2alpha/generation/image-to-video"
+	request, err := http.NewRequest("POST", url, body)
+	if err != nil {
+		fmt.Println("Can't create new request.", err)
+		return "", err
+	}
+	request.Header.Set("Content-Type", writer.FormDataContentType())
+	request.Header.Set("authorization", "Bearer "+vm.token)
+
+	// post request
+	client := &http.Client{}
+	response, err := client.Do(request)
+	if err != nil {
+		fmt.Println("Can't request with wrong request", err)
+		return "", err
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode == http.StatusOK {
+		fmt.Println("Success to request.")
+	} else if response.StatusCode >= 400 && response.StatusCode < 500 {
+		var errorMessage map[string]interface{}
+		err = json.NewDecoder(response.Body).Decode(&errorMessage)
+		if err != nil {
+			fmt.Println("Can't request with Non token", err)
+			return "", err
+		}
+	} else {
+		var errorMessage map[string]interface{}
+		err = json.NewDecoder(response.Body).Decode(&errorMessage)
+		if err != nil {
+			fmt.Println("Can't request with unknown error", err)
+			return "", err
+		}
+	}
+
+	// get generatedId
+	var buf bytes.Buffer
+	_, err = io.Copy(&buf, response.Body)
+	if err != nil {
+		fmt.Println("Can't read response body.", err)
+	}
+
+	return buf.String(), nil
 }
--- src/imagetovideo/stabilityai_test.go
+++ src/imagetovideo/stabilityai_test.go
@@ -1,22 +1,29 @@
 package imagetovideo
 
 import (
-	"bytes"
-	"encoding/json"
 	"fmt"
-	"io"
-	"mime/multipart"
-	"net/http"
 	"os"
 	"testing"
 
+	"github.com/joho/godotenv"
 	"github.com/stretchr/testify/assert"
 )
+
+func makeModule() *VideoManager {
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println("Error loading")
+	}
+	token := os.Getenv("STABILITY_AI_TOKEN_KEY")
+	vm := NewVideoManager("./img", token)
+	return vm
+}
 
 func TestCanResizeImage(t *testing.T) {
 	// given : 유효한 이미지
 	// when : 이미지 사이즈 변경 요청
-	vm := NewVideoManager("./img")
+	vm := makeModule()
+
 	resizedFilePath, _ := vm.resizeImage()
 
 	// then : 이미지 저장 파일 확인
@@ -24,87 +31,15 @@ func TestCanResizeImage(t *testing.T) {
 	assert.False(t, os.IsNotExist(err), "리사이즈된 파일이 정상적으로 생성되었습니다.")
 }
 
-func TestCanRequestGenerateVideo(t *testing.T) {
+func TestCanPostGenerateVideo(t *testing.T) {
 	// given : 유효한 토큰 + 이미지
 	// when : 비디오 생성 요청
-	resizedImagePath := "./img/resized_img.jpg"
-
-	// open file
-	file, err := os.Open(resizedImagePath)
-	if err != nil {
-		fmt.Println("Can't open resized file.", err)
-	}
-	defer file.Close()
-
-	// get fileInfo
-	fileInfo, err := file.Stat()
-	if err != nil {
-		fmt.Println("Can't get file info.", err)
-	}
-
-	// read fileInfo
-	fileBytes := make([]byte, fileInfo.Size())
-	_, err = file.Read(fileBytes)
-	if err != nil {
-		fmt.Println("Can't read file info.", err)
-	}
-
-	// put file data into buffer
-	body := &bytes.Buffer{}
-	writer := multipart.NewWriter(body)
-	formFile, err := writer.CreateFormFile("image", fileInfo.Name())
-	if err != nil {
-		fmt.Println("Can't create form file.", err)
-	}
-	_, err = io.Copy(formFile, bytes.NewReader(fileBytes))
-	if err != nil {
-		fmt.Println("Can't put file data into buffer.", err)
-	}
-	writer.Close()
-
-	// create request
-	token := ""
-	url := "https://api.stability.ai/v2alpha/generation/image-to-video"
-	request, err := http.NewRequest("POST", url, body)
-	if err != nil {
-		fmt.Println("Can't create new request.", err)
-	}
-	request.Header.Set("Content-Type", writer.FormDataContentType())
-	request.Header.Set("authorization", "Bearer "+token)
-
-	// post request
-	client := &http.Client{}
-	response, err := client.Do(request)
-	if err != nil {
-		fmt.Println("Can't request with wrong request", err)
-	}
-	defer response.Body.Close()
-
-	if response.StatusCode == http.StatusOK {
-		fmt.Println("Success to request.")
-	} else if response.StatusCode >= 400 && response.StatusCode < 500 {
-		var errorMessage map[string]interface{}
-		err = json.NewDecoder(response.Body).Decode(&errorMessage)
-		if err != nil {
-			fmt.Println("Can't request with Non token", err)
-		}
-	} else {
-		var errorMessage map[string]interface{}
-		err = json.NewDecoder(response.Body).Decode(&errorMessage)
-		if err != nil {
-			fmt.Println("Can't request with unknown error", err)
-		}
-	}
+	vm := makeModule()
+	id, _ := vm.postGenerateVideo()
 
 	// then : 생성 요청 확인(generated_id)
-	var buf bytes.Buffer
-	_, err = io.Copy(&buf, response.Body)
-	if err != nil {
-		fmt.Println("Can't read response body.", err)
-	}
-	fmt.Println("응답 값 : ", buf.String())
-
-	assert.Equal(t, response.StatusCode, http.StatusOK, "잘못된 Status code.")
+	fmt.Println("Generated id : ", id)
+	assert.True(t, len(id) > 0, "비디오 생성 요청이 정상적으로 수행 되었습니다.")
 }
 
 func TestCannotRequestGenerateVideoWithNonToken(t *testing.T) {
