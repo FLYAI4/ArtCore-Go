c6a951a8dffbca6b0cb7c585eac5cc81b80efbe3
robert-min <robertmin522@gmail.com>
2024-02-22 10:54:03 +0900

    test: ADD imageverification/verification resizeImage test code.

--- src/imageverification/verification_test.go
+++ src/imageverification/verification_test.go
@@ -1,13 +1,75 @@
 package imageverification
 
-import "testing"
+import (
+	"fmt"
+	"image"
+
+	"image/jpeg"
+	_ "image/png"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/nfnt/resize"
+	"github.com/stretchr/testify/assert"
+)
 
 func TestCanResizeImageWithJPG(t *testing.T) {
 	// given : 유효한 이미지(/storage/{generated_id}/origin.jpg)
-
 	// when : 이미지 리사이즈
 
+	// file open
+	filePath := filepath.Join("./img", "origin.jpg")
+
+	file, err := os.Open(filePath)
+	if err != nil {
+		fmt.Println("Can't open the file. : ", err)
+	}
+	defer file.Close()
+
+	// image decoding
+	img, _, err := image.Decode(file)
+	if err != nil {
+		fmt.Println("Can't decoding the image. : ", err)
+	}
+
+	// image resizing
+	changeWeight := 510
+	changeHeight := 680
+	resizeImg := resize.Resize(uint(changeWeight), uint(changeHeight), img, resize.Lanczos3)
+
+	// create new file
+	filePath = filepath.Join("./img", "resized.jpg")
+	newFile, err := os.Create(filePath)
+	if err != nil {
+		fmt.Println("Can't create new file. : ", err)
+	}
+	defer newFile.Close()
+
+	// save the image to file
+	err = jpeg.Encode(newFile, resizeImg, nil)
+	if err != nil {
+		fmt.Println("Can't save the image to file. : ", err)
+	}
+
 	// then : 리사이즈 이미지 생성(/storage/{generated_id}/resize.jpg)
+	filePath = filepath.Join("./img", "resized.jpg")
+	file, err = os.Open(filePath)
+	if err != nil {
+		fmt.Println("Can't open the file. : ", err)
+	}
+
+	img, _, err = image.Decode(file)
+	if err != nil {
+		fmt.Println("Can't decoding the image. : ", err)
+	}
+
+	width := img.Bounds().Dx()
+	height := img.Bounds().Dy()
+
+	fmt.Printf("이미지 크기: width = %d, height = %d\n", width, height)
+	assert.True(t, width == changeWeight, "크기가 정상적으로 변경되지 않았습니다.")
+	assert.True(t, height == changeHeight, "크기가 정상적으로 변경되지 않았습니다.")
 }
 
 func TestCanUpscaleImage(t *testing.T) {
